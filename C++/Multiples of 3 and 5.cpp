086afc8d51c2b057207f5c06620f3ecadbd7de3f
supremayro420 <56484801+supremayro420@users.noreply.github.com>
2020-05-09 16:59:07 +0300

    Create Multiples of 3 and 5.cpp
    i added those 99999999s thinking i'd need to print them as fizzbuzz but turns out i didnt, so i just commented them instead

---
+++ C++/Multiples of 3 and 5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> mainvec;
+	long int sum = 0;
+	for (int i = 0; i < 1000; i++) // generate the numbers inside vector
+	{
+		mainvec.push_back(i);
+	}
+	for (int i = 0; i < mainvec.size(); i++) // check for fizzbuzz
+	{
+		if (i % 3 == 0 && !i % 5 == 0) {
+			/*mainvec[i] = 99999993;*/
+			sum += i;
+		}
+		else if (i % 3 == 0)
+		{
+			/*mainvec[i] = 99999991;*/
+			sum += i;
+		}
+		else if (i % 5 == 0)
+		{
+			/*mainvec[i] = 99999992;*/
+			sum += i;
+		}
+	}
+	printf("Sum of these:: %li\n", sum);
+	return EXIT_SUCCESS;
+}
